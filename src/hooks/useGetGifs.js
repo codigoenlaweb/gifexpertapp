50efa21ea8bc9998df56d7f2a29c811600894e0f
codigoenlaweb <codeenlaweb@gmail.com>
2022-04-10 07:51:08 -0400

    error dependeci useEfect

--- src/hooks/useGetGifs.js
+++ src/hooks/useGetGifs.js
@@ -5,21 +5,18 @@ export const useGetGifs = (category) => {
   const [state, setState] = useState({ gifs: [], load: false });
 
   useEffect(() => {
-    getGifs(category).then((dat) => setState({gifs:dat, load:true}));
+    getGifs(category).then((dat) => setState({ gifs: dat, load: true }));
   }, [category]);
 
   useEffect(() => {
-    function isLoadTrue(state) {
-      if (state.load) {
-        document.querySelector('#grid-card').classList.remove('opacity-0')
-        document.querySelector('#grid-card').classList.add('opacity-100')
-        if (state.gifs.length === 0) {
-          document.querySelector('#any-gif').classList.remove('hidden')
-        }
+    console.log('hi');
+    if (state.load) {
+      document.querySelector("#grid-card").classList.remove("opacity-0");
+      document.querySelector("#grid-card").classList.add("opacity-100");
+      if (state.gifs.length === 0) {
+        document.querySelector("#any-gif").classList.remove("hidden");
       }
     }
-    
-    isLoadTrue(state)
-  }, [state.load]);
-  return state
+  }, [state]);
+  return state;
 };
