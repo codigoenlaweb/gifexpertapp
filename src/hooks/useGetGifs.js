6dba6717f90220e2e2e4d478ddca521527df51fd
codigoenlaweb <codeenlaweb@gmail.com>
2022-04-10 00:21:47 -0400

    error build ready

--- src/hooks/useGetGifs.js
+++ src/hooks/useGetGifs.js
@@ -8,10 +8,11 @@ export const useGetGifs = (category) => {
     getGifs(category).then((dat) => setState({gifs:dat, load:true}));
   }, [category]);
 
-  useEffect(() => {
+  useEffect((state) => {
     if (state.load) {
       document.querySelector('#grid-card').classList.remove('opacity-0')
       document.querySelector('#grid-card').classList.add('opacity-100')
+      console.log(state);
       if (state.gifs.length === 0) {
         document.querySelector('#any-gif').classList.remove('hidden')
       }
